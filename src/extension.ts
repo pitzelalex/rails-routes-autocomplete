c3975565a1d9cad903f3e0022f211578de63e5c2
Alex Pitzel <pitzelalex@gmail.com>
2023-04-17 14:43:55 -0400

    Add rails console commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { spawn } from "child_process";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,6 +10,29 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "railsroutesautocomplete" is now active!');
+
+	// use workspace to listen to changes to routes file - Thomas
+	// let railsCommand = vscode.workspace.getConfiguration().get("railsRoutes");
+	let workspaceFolder = vscode.workspace.workspaceFolders;
+
+	if(!workspaceFolder) {
+		console.log("Couldn't find workspace");
+		return;
+	}
+
+	let process = spawn('rails', ['c'], {
+		cwd: workspaceFolder[0].uri.fsPath
+	});
+	process.stdin.write("Rails.application.routes.named_routes.helper_names\n");
+	let outputChannel = vscode.window.createOutputChannel('rails routes');
+
+	process.stdout.on('data', (data) => {
+		console.log(data.toString());
+    outputChannel.appendLine(data);
+  });
+	outputChannel.show();
+
+	vscode.languages.registerCompletionItemProvider()
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
