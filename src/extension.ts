3b2575f9c123ac645010d0f80acc9ac37e057406
Thomas Casady <tscasady@gmail.com>
2023-05-17 15:39:09 -0500

    Add watcher to routes file.

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,6 @@ import * as vscode from 'vscode';
 import { exec } from "child_process";
 
 export function activate(context: vscode.ExtensionContext) {
-	// use workspace to listen to changes to routes file - Thomas
-	// let railsCommand = vscode.workspace.getConfiguration().get("railsRoutes");
 	let workspaceFolder = vscode.workspace.workspaceFolders;
 
 	if (!workspaceFolder) {
@@ -11,16 +9,30 @@ export function activate(context: vscode.ExtensionContext) {
 		return;
 	}
 
+	addRouteAutoCmp(context, workspaceFolder);
+
+	const routesFileWatcher = vscode.workspace.createFileSystemWatcher('**/config/routes.rb');
+	context.subscriptions.push(routesFileWatcher);
+	routesFileWatcher.onDidChange(() => {
+		addRouteAutoCmp(context, workspaceFolder as readonly vscode.WorkspaceFolder[]);
+});
+
 	// Check if the current workspace is a Rails project
 	// const isRailsProject = fs.existsSync(path.join(workspaceFolder, 'Gemfile'));
 
-	let process = exec(`echo 'Rails.application.routes.named_routes.helper_names' | bundle exec rails c`, { cwd: workspaceFolder[0].uri.fsPath }, (err, stdout, stderr) => {
+}
+
+function sanitizeRouteHelper(routeHelper: string) {
+	return routeHelper.replace(/['"\s\[\]]+/g, '');
+}
+
+async function addRouteAutoCmp(context: vscode.ExtensionContext, workspaceFolder: readonly vscode.WorkspaceFolder[]){
+	exec(`echo 'Rails.application.routes.named_routes.helper_names' | bundle exec rails c`, { cwd: workspaceFolder[0].uri.fsPath }, (err, stdout, stderr) => {
 		let provider = vscode.languages.registerCompletionItemProvider("ruby", {
 			provideCompletionItems() {
 				const completionItems: vscode.CompletionItem[] = [];
 				for (let method of stdout.split(',').slice(1)) {
 					method = sanitizeRouteHelper(method);
-					console.log(method);
 					const item = new vscode.CompletionItem(method, vscode.CompletionItemKind.Method);
 					item.insertText = method;
 					completionItems.push(item);
@@ -30,11 +42,6 @@ export function activate(context: vscode.ExtensionContext) {
 		});
 		context.subscriptions.push(provider);
 	});
-
-}
-
-function sanitizeRouteHelper(routeHelper: string) {
-	return routeHelper.replace(/['"\s\[\]]+/g, '');
 }
 
 export function deactivate() { }
